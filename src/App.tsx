56a551b72f83afa4eb3c0fae6b90050d8e080544
Mohamed Ghonaim <mohamed.ghonim21@gmail.com>
2025-08-10 19:02:32 +0300

    Remove extra closing div tag

--- src/App.tsx
+++ src/App.tsx
@@ -1408,7 +1408,6 @@ const App: React.Component = () => {
                     <option value="Night Shift (7:00 PM - 7:00 AM)">Night Shift (7:00 PM - 7:00 AM)</option>
                   </select>
                 </div>
-                </div>
               </div>
             </div>
           </div>
